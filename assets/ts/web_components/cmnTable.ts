e071fd6c8598c58c44cdca18e2d36d9d44d5d814
Kitamura <hinitial@gmail.com>
2023-05-06 18:17:56 +0900

    普通に値を渡せた

--- assets/ts/web_components/cmnTable.ts
+++ assets/ts/web_components/cmnTable.ts
@@ -11,15 +11,30 @@ export class CmnTable extends LitElement {
   `;
 
   @property({ type: Array })
-  items = ["World", "aaa"];
+  fields: { key: string; label: string }[] = [];
+
+  @property({ type: Array })
+  items: Record<string, string | number>[] = [];
 
   // Render the UI as a function of component state
   render() {
     return html`
       <table>
-        <tr>
-          ${this.items.map((item) => html`<td>${item}</td>`)}
-        </tr>
+        <thead>
+          <tr>
+            ${this.fields.map((field) => html`<th>${field.label}</th>`)}
+          </tr>
+        </thead>
+        <tbody>
+          ${this.items.map(
+            (item) =>
+              html`<tr>
+                ${this.fields.map(
+                  (field) => html`<td>${item[field.key] || ""}</td>`
+                )}
+              </tr>`
+          )}
+        </tbody>
       </table>
     `;
   }
